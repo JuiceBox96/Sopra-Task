afdc261e0c621cf6b42433244c3ecf3904550559
JuiceBox96 <75124951+JuiceBox96@users.noreply.github.com>
2022-02-16 12:02:12 +0000

    created initial function getIntersection

--- task.js
+++ task.js
@@ -1,2 +1,15 @@
 
+/*Write a function, GetIntersection which returns the intersection of these two interval lists.
+Write a test method which passes the supplied examples.*/
 
+
+function GetIntersection () {
+    
+    // let variable used in this case as firstList and secondList will be compared//
+    let firstList = 0;
+    let secondList = 0;
+
+
+
+
+}
