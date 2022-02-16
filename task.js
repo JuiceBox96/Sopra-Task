98ca427c5302dc9654cf60830869e7eecd34780d
JuiceBox96 <75124951+JuiceBox96@users.noreply.github.com>
2022-02-16 12:12:48 +0000

    amended variables to listOne and listTwo

--- task.js
+++ task.js
@@ -3,11 +3,12 @@
 Write a test method which passes the supplied examples.*/
 
 
-function GetIntersection () {
+function GetIntersection (firstList, secondList) {
     
-    // let variable used in this case as firstList and secondList will be compared//
-    let firstList = 0;
-    let secondList = 0;
+    //let variable used in this case as listOne and listTwo will be compared.//
+    //Came to this conclusion as both are expecting outputs 
+    let listOne = 0;
+    let listTwo = 0;
 
 
 
