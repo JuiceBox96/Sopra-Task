5fb0b805109d6f8ce84b5b5012b04bf730e5db81
JuiceBox96 <75124951+JuiceBox96@users.noreply.github.com>
2022-02-21 23:59:29 +0000

    further test amending

--- test.js
+++ test.js
@@ -2,40 +2,36 @@
 //Node module, assert can be imported at the top of your files
 const assert = require('assert');
 
-
+// test for Math. functions
 describe ('getInterSection', () => {
     it('returns nest variables, while loop and if statement')
 
     describe('Math', () => {
         describe('.min', () => {
-          it('returns the argument with the lowest value', () => {
+          it('returns the array with the lowest value', () => {
       
           })
         })
       })
-
-      
 
       describe('Math', () => {
         describe('.max', () => {
-          it('returns the argument with the high value', () => {
-      
+          it('returns the array with the highest value', () => {
           })
         })
       })
-})
+    })
 
 
 
+// test for if statement increments 
 describe('one++', () => {
     it('returns the sum of its arguments', () => {
-      assert.ok
     });
   });
 
   describe('two++', () => {
     it('returns the sum of its arguments', () => {
-      assert.ok
     });
   });
 
